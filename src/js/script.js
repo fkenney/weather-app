b2df2a115c3d2565d4f6fa2f0210a57d7087e158
Felicia Kenney <fkenney@bravocg.com>
2022-08-24 11:07:20 -0400

    updated units to imperial metric updated css file

--- src/js/script.js
+++ src/js/script.js
@@ -10,7 +10,7 @@ document.getElementById("searchbox").addEventListener("keyup", (event)=>{
 var weather = {
     "apiKey": "ce52c73424dd223d64efe36faa2d6dbf",
     fetchWeather : function(city){
-        fetch("https://api.openweathermap.org/data/2.5/weather?q="+city + "&appid="+ this.apiKey)
+        fetch("https://api.openweathermap.org/data/2.5/weather?units=imperial&q="+city + "&appid="+ this.apiKey)
         .then(response => response.json())
         .then(data => this.display(data))
      },
@@ -19,16 +19,16 @@ var weather = {
         const {name} = data;
         const {icon, description} = data.weather[0];
         const {temp, humidity} = data.main;
-        const { speed } = data.wind;
+        const { speed } = data.wind ;
 
         console.log(name, icon, description, temp, humidity, speed);
 
         document.getElementById("cityName").innerText=name;
         document.getElementById("icon").src = "http://openweathermap.org/img/wn/" + icon +"@2x.png";
-        document.getElementById("temp").innerText = temp;
+        document.getElementById("temp").innerText = temp + " °F";
         document.getElementById("desc").innerText = description;
-        document.getElementById("humidity").innerText = humidity;
-        document.getElementById("speed").innerText = speed;
+        document.getElementById("humidity").innerText = humidity +" %";
+        document.getElementById("speed").innerText = speed +" mph";
         
      }
 };
